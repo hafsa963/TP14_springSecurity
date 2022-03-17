743553fc743227180cfe5951cbcaf0097f0986c4
hafsa <hafsa@DESKTOP-LHJFQJ8>
2022-03-18 00:41:36 +0100

    first commit

---
+++ src/test/java/cigma/pfe/frontend/FrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package cigma.pfe.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
